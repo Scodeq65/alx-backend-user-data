78ab8ed350877abb11e0d6db44264a6c49ccb799
scodeq65 <sodiqagbaraojo@gmail.com>
2024-11-13 05:31:59 +0100

    forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -29,3 +29,8 @@ def stats() -> str:
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    abort(403)
