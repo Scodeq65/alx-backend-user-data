3ef0724087c2a8b3d5193d6369246caf106cec92
scodeq65 <sodiqagbaraojo@gmail.com>
2024-11-18 13:12:55 +0100

    modified

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -26,7 +26,7 @@ def view_one_user(user_id: str = None) -> str:
       - 404 if the User ID doesn't exist or
       if "me" and no authenticated user.
     """
-    if user_is is None:
+    if user_id is None:
         abort(404)
 
     # Handle the "me" case for the authenticated user
@@ -34,7 +34,7 @@ def view_one_user(user_id: str = None) -> str:
         if request.current_user is None:
             abort(404)
         return jsonify(request.current_user.to_json())
-    
+
     # Handle standard user lookup by user_id
     user = User.get(user_id)
     if user is None:
