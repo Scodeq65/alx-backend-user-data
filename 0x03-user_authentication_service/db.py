3bb89fce2845f59a1bcd8b1d94b4d1626eb93b77
scodeq65 <sodiqagbaraojo@gmail.com>
2024-11-21 21:32:30 +0100

    update_usr

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ save the user to the database and returns a User object 
+        """ save the user to the database and returns a User object
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
